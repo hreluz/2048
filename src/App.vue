0f696ba2c8b9f86350ac6f9bff71c794e5ed8e0c
Hector <hmreluz@gmail.com>
2017-10-22 03:24:30 -0500

    Install Vue.js with vue-cli

---
+++ src/App.vue
@@ -0,0 +1,88 @@
+<template>
+  <div id="app">
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-3 col-lg-3 no-padding-right no-padding-left box">
+                <div class="number">2048</div>
+            </div>
+
+            <div class="col-xs-3 col-lg-3 no-padding-right no-padding-left box">
+                <div class="number">2048</div>
+            </div>
+
+            <div class="col-xs-3 col-lg-3 no-padding-right no-padding-left box">
+                <div class="number">2048</div>
+            </div>
+
+            <div class="col-xs-3 col-lg-3 no-padding-right no-padding-left box">
+                <div class="number">2048</div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-xs-3 col-lg-3 no-padding-right no-padding-left box">
+                <div class="number">2048</div>
+            </div>
+
+            <div class="col-xs-3 col-lg-3 no-padding-right no-padding-left box">
+                <div class="number">2048</div>
+            </div>
+
+            <div class="col-xs-3 col-lg-3 no-padding-right no-padding-left box">
+                <div class="number">2048</div>
+            </div>
+
+            <div class="col-xs-3 col-lg-3 no-padding-right no-padding-left box">
+                <div class="number">2048</div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-xs-3 col-lg-3 no-padding-right no-padding-left box">
+                <div class="number">2048</div>
+            </div>
+
+            <div class="col-xs-3 col-lg-3 no-padding-right no-padding-left box">
+                <div class="number">2048</div>
+            </div>
+
+            <div class="col-xs-3 col-lg-3 no-padding-right no-padding-left box">
+                <div class="number">2048</div>
+            </div>
+
+            <div class="col-xs-3 col-lg-3 no-padding-right no-padding-left box">
+                <div class="number">2048</div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-xs-3 col-lg-3 no-padding-right no-padding-left box">
+                <div class="number">2048</div>
+            </div>
+
+            <div class="col-xs-3 col-lg-3 no-padding-right no-padding-left box">
+                <div class="number">2048</div>
+            </div>
+
+            <div class="col-xs-3 col-lg-3 no-padding-right no-padding-left box">
+                <div class="number">2048</div>
+            </div>
+
+            <div class="col-xs-3 col-lg-3 no-padding-right no-padding-left box">
+                <div class="number">2048</div>
+            </div>
+        </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
